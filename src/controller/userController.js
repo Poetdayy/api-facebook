05617cf7ab98771599f9413f61983fef609bb728
hieubt <80142234+hieutbui@users.noreply.github.com>
2023-02-06 11:45:39 +0700

    ADD:set_request_friend+get_list_blocks

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,6 +1,7 @@
 // common function
 import UserModel from '../models/users';
 import { verifyJwtToken } from '../utils';
+import AccountModel from '../models/accounts';
 
 /**
  * @author hieubt
@@ -46,7 +47,12 @@ const set_accept_friend = async (req, res) => {
             .then(async (result) => {
               if (!result) {
                 return res.json({
-                  code: 9995,
+                  code: '9995',
+                  message: 'User is not validated',
+                });
+              } else if (result?.is_blocked) {
+                return res.json({
+                  code: '9995',
                   message: 'User is not validated',
                 });
               } else {
@@ -79,10 +85,9 @@ const set_accept_friend = async (req, res) => {
           return res.json({
             code: '1009',
             message: 'Not access',
-            error: err,
           });
         });
-    } catch (err) {
+    } catch (error) {
       return res.json({
         code: '1005',
         message: 'Unknown error',
@@ -91,7 +96,155 @@ const set_accept_friend = async (req, res) => {
   }
 };
 
+//TODO
+const get_list_suggested_friend = async (req, res) => {};
+
+/**
+ * @description need test
+ * @author hieubt
+ * @param {Object} req
+ * @param {Object} res
+ * @returns {JSON}
+ */
+const set_request_friend = async (req, res) => {
+  const { token, user_id } = req.body;
+  if (!token || !user_id) {
+    return res.json({
+      code: '1002',
+      message: 'Parameter is not enough',
+    });
+  } else {
+    await verifyJwtToken(token, process.env.jwtSecret)
+      .then(async () => {
+        await AccountModel.findOne({ token: token })
+          .then(async (response) => {
+            if (response) {
+              return res.json({
+                code: '1004',
+                message: 'Parameter value is invalid',
+              });
+            } else if (response.friendIds.length >= process.env.friendLimit) {
+              return res.json({
+                code: '9994',
+                message: 'No data or end of list data',
+              });
+            } else {
+              await UserModel.findOne({ id: user_id })
+                .then(async (data) => {
+                  if (!data) {
+                    return res.json({
+                      code: '9995',
+                      message: 'User is not validated',
+                    });
+                  } else if (data?.is_blocked) {
+                    return res.json({
+                      code: '9995',
+                      message: 'User is not validated',
+                    });
+                  } else {
+                    if (!data?.friendRequestIds?.includes(user_id)) {
+                      res.friendRequestIds.push(user_id);
+                    }
+                    await data.save();
+                    return res.json({
+                      code: '1000',
+                      message: 'OK',
+                      data: res.friendRequestIds.length,
+                    });
+                  }
+                })
+                .catch((err) => {
+                  return res.json({
+                    code: '1005',
+                    message: 'Unknown error',
+                  });
+                });
+            }
+          })
+          .catch((err) => {
+            return res.json({
+              code: '1005',
+              message: 'Unknown error',
+            });
+          });
+      })
+      .catch((err) => {
+        return res.json({
+          code: '1005',
+          message: 'Unknown message',
+        });
+      });
+  }
+};
+
+/**
+ * @description need test
+ * @author hieubt
+ * @param {Object} req
+ * @param {Object} res
+ * @returns {JSON}
+ */
+const get_list_blocks = async (req, res) => {
+  const { token, index, count } = req.body;
+  if (!token || !index || !count) {
+    return res.json({
+      code: '1002',
+      message: 'Parameter is not enough',
+    });
+  } else {
+    await verifyJwtToken(token, process.env.jwtSecret)
+      .then(async () => {
+        await AccountModel.findOne({ token: token })
+          .then(async (result) => {
+            await getUserByAccountId(result._id.toString())
+              .then((data) => {
+                if (!data.found) {
+                  return res.json({
+                    code: '1005',
+                    message: 'Unknown error',
+                  });
+                } else {
+                  let blockedList = [];
+                  for (let i = index; i <= count; i++) {
+                    blockedList.push({
+                      id: data?.foundUser?.id,
+                      name: data?.foundUser?.username,
+                      avatar: data?.foundUser?.avatar,
+                    });
+                  }
+                  return res.json({
+                    code: '1000',
+                    message: 'OK',
+                    data: JSON.stringify(blockedList),
+                  });
+                }
+              })
+              .catch((error) => {
+                return res.json({
+                  code: '1005',
+                  message: 'Unknown error',
+                });
+              });
+          })
+          .catch((error) => {
+            return res.json({
+              code: '1005',
+              message: 'Unknown error',
+            });
+          });
+      })
+      .catch((err) => {
+        return res.json({
+          code: '1005',
+          message: 'Unknown error',
+        });
+      });
+  }
+};
+
 module.exports = {
   getUserByAccountId,
   set_accept_friend,
+  set_request_friend,
+  get_list_blocks,
 };
